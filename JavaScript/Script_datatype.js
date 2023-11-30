4ce5d52be5ff72813d298739b60de1184b1fb421
Mohit Kumar Jaiswal <119107584+Mohitkrjswl@users.noreply.github.com>
2023-11-30 11:00:15 +0000

    Datatype

---
+++ JavaScript/Script_datatype.js
@@ -0,0 +1,13 @@
+"Use Strict";// treat all js code as a new version
+// primitive datatype are:- 
+/*Number: Represents numeric values (e.g., 1, 3.14).
+String: Represents sequences of characters (e.g., "hello").
+Boolean: Represents true or false values.
+Null: Represents the intentional absence of any object value.
+Undefined: Represents an uninitialized variable.
+Symbol: Introduced in ECMAScript 6 (ES6), represents a unique identifier.
+Object (Non-primitive) Data Types:
+
+Object: A collection of key-value pairs, often used for complex data structures.
+Array: An ordered collection of values, accessed using numerical indices.
+// Function: A reusable block of code that can be invoked by its name.*/
---
+++ JavaScript/Script_variables.js
@@ -0,0 +1,9 @@
+const accountId= 12345
+let accountEmail = "Mohitkr@google.com"
+var accountPassword= "Mohit@google"
+accountCity= "Delhi"
+
+accountEmail= "Mohit@google"
+accountPassword="123"
+accountCity= "Mumbai"
+console.table([accountEmail,accountPassword,accountCity])
