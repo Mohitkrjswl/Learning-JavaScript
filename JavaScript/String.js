8b5598a7732d2c3e6a9e77ce212e6a0ad1cf42b3
Mohit Kumar Jaiswal <119107584+Mohitkrjswl@users.noreply.github.com>
2023-11-30 23:45:28 +0530

    Update String.js

--- JavaScript/String.js
+++ JavaScript/String.js
@@ -1,11 +1,11 @@
-const name = "hitesh"
-const repoCount = 50
+const name = "Mohit kr"
+const repoCount = 55
 
 // console.log(name + repoCount + " Value");
 
 console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('mohit-hc-com')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -22,11 +22,10 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   mohit    "
 console.log(newStringOne);
 console.log(newStringOne.trim());
 
-const url = "https://hitesh.com/hitesh%20choudhary"
 
 console.log(url.replace('%20', '-'))
 
