7a4cb5cd636e9dcb5a9f4c5e5b37118a78d7f9dc
Mohit Kumar Jaiswal <119107584+Mohitkrjswl@users.noreply.github.com>
2023-12-01 14:02:21 +0000

    numbers

---
+++ JavaScript/number-maths.js
@@ -0,0 +1,14 @@
+// const score= 500
+// console.log(score);
+// const balance=  new Number(100)
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(typeof balance);
+// console.log(balance.toFixed(2));
+// const otherNumber= 123.67
+// console.log(otherNumber.toPrecision(4));
+// const number= 10000000
+// console.log(number.toLocaleString('en-IN'));
+// ++++++++++++++++ Maths +++++++++++++++++++++++
+
+
