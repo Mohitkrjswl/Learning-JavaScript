d5ada9597ffd08b4c4b0a8a27ce3cc96e0f42c71
Mohit Kumar Jaiswal <119107584+Mohitkrjswl@users.noreply.github.com>
2023-12-03 17:41:46 +0000

    function

---
+++ JavaScript/function.js
@@ -0,0 +1,21 @@
+/* Function is a set of codes which we writes to perform
+ a perticular task and we can also recall it asper our requiremen*/
+// Declaration of function
+// function greet(){
+//     console.log("Hello everyone");
+// }
+// const greeting= setInterval (greet,3000);
+// console.log(greeting);
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// function expression
+// when we want to assign a function to a variable.
+//  const word= function(){
+//     console.log("this is function expression");
+
+//  }
+//  word();
+const add= function(x){
+    return x+x;
+
+}
+console.log(add(6));
