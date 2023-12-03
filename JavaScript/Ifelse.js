7b2b6617e94074a89c40987ebddf45f3136b0a41
Mohit Kumar Jaiswal <119107584+Mohitkrjswl@users.noreply.github.com>
2023-12-03 17:01:12 +0000

    ifelse added

---
+++ JavaScript/Ifelse.js
@@ -0,0 +1,21 @@
+// If Else statement
+/*if (condition){
+    // Code will run if the condition is true/
+}
+else{
+    code will run if the condition is not true
+ }*/
+//  const age = 25;
+//  if ( age===26 ){
+//     console.log("Age is ",age);
+// } else {
+//     console.log("Not Matched");
+// }
+const age = 25;
+ if ( age>=27 ){
+    console.log("Age is ",age);
+} else if (age==25){
+    console.log("Correct Age")}
+else {
+    console.log("Not Matched");
+}
