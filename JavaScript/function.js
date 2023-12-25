2191b7f370aadea2051031c762fa74e8d3a66111
Mohit Kumar Jaiswal <119107584+mohitkrjswl@users.noreply.github.com>
2023-12-25 06:45:07 +0000

    more

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -20,22 +20,35 @@
 // }
 // console.log(add(6));
 // Function with a callback
-function greet(name, callback) {
-    console.log("Hello, " + name + "!");
+// function greet(name, callback) {
+//     console.log("Hello, " + name + "!");
   
-    // Simulate a task that takes some time (e.g., fetching data)
-    setTimeout(function () {
-      console.log("Task completed!");
-      // Execute the callback function
-      callback();
-    }, 2000); // Simulating a 2-second delay
+//     // Simulate a task that takes some time (e.g., fetching data)
+//     setTimeout(function () {
+//       console.log("Task completed!");
+//       // Execute the callback function
+//       callback();
+//     }, 2000); // Simulating a 2-second delay
+//   }
+  
+//   // Callback function
+//   function sayGoodbye() {
+//     console.log("Goodbye!");
+//   }
+  
+//   // Using the greet function with a callback
+//   greet("Alice", sayGoodbye);
+  function square(n){
+    return n*n;
   }
-  
-  // Callback function
-  function sayGoodbye() {
-    console.log("Goodbye!");
+  function cube(n){
+    return n*n*n;
   }
-  
-  // Using the greet function with a callback
-  greet("Alice", sayGoodbye);
-  
+  function sumofsomething (a,b, callbackfn){
+    let square1 = callbackfn(a);
+    let square2 = callbackfn(b);
+    return square1+square2;
+
+  }
+ let ans= (2,3,square);
+ console.log(ans);
