2a6eae165910a66b095b9ca0ac64916e450b7c67
Mohit Kumar Jaiswal <mohitkrjswl@gmail.com>
2024-01-16 21:45:36 +0530

    Calculator project

---
+++ Revision/script.js
@@ -0,0 +1,11 @@
+console.log('I am coming from Index.js');
+let welcomeMessage ='Hello this a welcome message'
+
+console.log('Hello Everyone!');
+console.log(welcomeMessage);
+// let a=5;
+// let b=6;
+// console.log(a+b);
+// let c= a+7;
+// console.log(c);
+
