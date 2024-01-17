366145bddc88e75618d37dcbab3dd2b319da0cdf
Mohit Kumar Jaiswal <mohitkrjswl@gmail.com>
2024-01-17 11:50:30 +0530

    Cricket Project Uploaded

--- Revision/script.js
+++ Revision/script.js
@@ -1,11 +1,20 @@
-console.log('I am coming from Index.js');
-let welcomeMessage ='Hello this a welcome message'
+// console.log('I am coming from Index.js');
+// let welcomeMessage ='Hello this a welcome message'
 
-console.log('Hello Everyone!');
-console.log(welcomeMessage);
-// let a=5;
-// let b=6;
-// console.log(a+b);
-// let c= a+7;
-// console.log(c);
+// console.log('Hello Everyone!');
+// console.log(welcomeMessage);
+// // let a=5;
+// // let b=6;
+// // console.log(a+b);
+// // let c= a+7;
+// // console.log(c);
 
+console.log("Checking if you can drive-");
+let age = 75;
+if(age>70){
+  console.log('Please! Ask someone else to drive for you');
+} else if(age>18){ console.log('Conngrats! You can drive');
+}
+else{
+  console.log('Sorry! You can use a bicycle.');
+}
