eb456314b371248ebac911fa555348b30d996039
Mohit Kumar Jaiswal <mohitkrjswl@gmail.com>
2024-01-24 16:32:47 +0530

    first Commit

---
+++ Revision/MyntraProject/index.js
@@ -0,0 +1,21 @@
+let itemsContainerElement = document.querySelector(".items-container");
+
+let item={
+    
+}
+
+itemsContainerElement.innerHTML = ` 
+<div class="item-container">
+<img class="item-image" src="1.jpg" alt="item img">
+<div class="rating">
+    4.5⭐ | 1.4k
+</div>
+<div class="company-name">Carlton London</div> <div class="item-name">Rhodium-Plated CZ Floral Studs</div>
+<div class="price">
+    <span class="current-price">Rs 606</span>
+    <span class="original-price">Rs 1045</span>
+    <span class="discount-percentage">(42% OFF)</span>
+</div>
+<button class="btn-add-bag">Add to Bag</button>
+</div>
+</div>`;
