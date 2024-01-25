9136a6ad4f0630f76dad5165b0e13e32cd09767c
Mohit Kumar Jaiswal <mohitkrjswl@gmail.com>
2024-01-25 22:53:23 +0530

    finall

--- Revision/MyntraProject/index.js
+++ Revision/MyntraProject/index.js
@@ -1,21 +1,55 @@
-let itemsContainerElement = document.querySelector(".items-container");
+let bagItems;
+onLoad();
 
-let item={
-    
+function onLoad(){
+ let bagItemsStr = localStorage.getItem('bagItems');
+ bagItems = bagItemsStr ? JSON.parse(bagItemsStr) : [];
+displayItemsOnHomePage();
+displayBagIcon();
 }
 
-itemsContainerElement.innerHTML = ` 
-<div class="item-container">
-<img class="item-image" src="image/1.jpg" alt="item img">
-<div class="rating">
-    4.5⭐ | 1.4k
-</div>
-<div class="company-name">Carlton London</div> <div class="item-name">Rhodium-Plated CZ Floral Studs</div>
-<div class="price">
-    <span class="current-price">Rs 606</span>
-    <span class="original-price">Rs 1045</span>
-    <span class="discount-percentage">(42% OFF)</span>
-</div>
-<button class="btn-add-bag">Add to Bag</button>
-</div>
-</div>`;
+function addToBag(itemId) {
+bagItems.push(itemId);
+localStorage.setItem('bagItems', JSON.stringify(bagItems));
+displayBagIcon();
+}
+
+function displayBagIcon(){
+    let bagItemCountElement = document.querySelector('.bag-item-count');
+    if (bagItems.length > 0){
+    bagItemCountElement.innerText = bagItems.length;
+    bagItemCountElement.style.visibility = 'visible';
+    } else {
+        bagItemCountElement.style.visibility = 'hidden';
+    }
+
+}
+function displayItemsOnHomePage(){
+    let itemsContainerElement = document.querySelector(".items-container");
+    if(!itemsContainerElement){
+        return;
+    }
+
+    let innerHTML='';
+    items.forEach(item =>{
+        innerHTML += ` 
+        <div class="item-container">
+        <img class="item-image" src="${item.image}" alt="item image">
+        <div class="rating">
+            ${ item.rating.stars} ⭐ |${item.rating.count}
+        </div>
+        <div class="company-name">${item.company}</div> <div class="item-name">${item.item_name}</div>
+        <div class="price">
+            <span class="current-price">Rs ${item.current_price}</span>
+            <span class="original-price">Rs ${item.original_price}</span>
+            <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
+        </div>
+        <button class="btn-add-bag" onclick="addToBag(${item.id})">Add to Bag</button>
+        </div>
+        </div>`
+    });
+    
+    itemsContainerElement.innerHTML=innerHTML;
+}
+
+
